f61ce24fd9cc6d7cb78b10e6510e36b60fd055bf
Geraldo Luiz <geraldobl58@gmail.com>
2022-10-11 18:21:19 -0300

    feat: Add token configuration

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,11 @@
 export * from './colors'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+
+export * from './line-heights'
+
+export * from './radii'
+
+export * from './space'
